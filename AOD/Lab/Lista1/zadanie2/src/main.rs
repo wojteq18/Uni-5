1893aba713270ec3ba9b97be9ed5c0eea879f464
wojteq18 <279730@student.pwr.edu.pl>
2025-10-21 14:21:41 +0200

    AOD L1/ ex2

--- AOD/Lab/Lista1/zadanie2/src/main.rs
+++ AOD/Lab/Lista1/zadanie2/src/main.rs
@@ -3,8 +3,6 @@ use std::fs::File;
 use std::error::Error;
 use std::io::BufRead;
 use std::io::Write;
-use std::env;
-use std::process;
 
 
 fn dfs_visit(
@@ -12,43 +10,50 @@ fn dfs_visit(
     adj_list: &Vec<Vec<usize>>,
     u: usize, 
     parent: &mut Vec<usize>,
-    order: &mut usize,
-) {
+    order: &mut Vec<usize>,
+) -> bool {
     color[u] = 1; // Gray
-    print!("-> {}", u);
 
     for v in &adj_list[u] {
         if color[*v] == 0 { 
-            *order += 1;
             parent[*v] = u;
-            dfs_visit(color, adj_list, *v, parent, order);
+            if !dfs_visit(color, adj_list, *v, parent, order) {
+                return false;
+            }
+        } else if color[*v] == 1 {
+            // Found a back edge, indicating a cycle
+            return false;
         }
     }
     color[u] = 2; // Black
+    order.push(u);
+    return true
     }
 
-fn dfs(num_vertex: &usize, adj_list: &Vec<Vec<usize>>, print_tree: &bool) {
+fn dfs_top(num_vertex: usize, adj_list: &Vec<Vec<usize>>) {
     let mut color: Vec<usize> = vec![0; num_vertex + 1]; // 0:white, 1:gray, 2:black
     let mut parent: Vec<usize> = vec![0; num_vertex + 1]; // 0 means no parent
-    let mut order: usize = 0; // Edge discovery order counter
+    let mut order: Vec<usize> = Vec::with_capacity(num_vertex); //vec to store topological order
 
 
-    for u in 1..=*num_vertex {
+    for u in 1..=num_vertex {
         if color[u] == 0 { 
-            dfs_visit(&mut color, adj_list, u, &mut parent, &mut order);
-        }
-    }
-
-    if *print_tree {
-        let mut file = File::create("dfs_tree.txt").expect("Impossible to create file");
-        let mut temp_order = 0;
-        for v in 1..=*num_vertex {
-            if parent[v] != 0 { // If vertex 'v' has a parent
-                temp_order += 1;
-                writeln!(file, "{} {} {}", parent[v], v, temp_order).expect("Impossible to write to file");
+            if !dfs_visit(&mut color, adj_list, u, &mut parent, &mut order) {
+                println!("Graph contains a cycle. Topological sorting not possible.");
+                return;
             }
         }
     }
+
+    order.reverse(); // Reverse to get the correct topological order
+    println!("Graph is a DAG");
+
+    if num_vertex <= 200 {
+        let mut file = File::create("topological.txt").expect("Impossible to create file");
+        for v in order.iter() {
+            writeln!(file, "{}", v).expect("Impossible to write to file");
+        }
+    }    
 }
 
 fn parse_simple_format(file_path: &str) -> Result<(usize, Vec<Vec<usize>>), Box<dyn Error>> {
@@ -78,12 +83,6 @@ fn parse_simple_format(file_path: &str) -> Result<(usize, Vec<Vec<usize>>), Box<
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <Y or N>", args[0]);
-        process::exit(1);
-    }
-    let tree = &args[1];
     let (num_vex, adj_list) = parse_simple_format("/home/wojteq18/sem5/AOD/Lab/Lista1/graph_for_rust.txt").unwrap();
-    dfs(&num_vex, &adj_list, &(tree == "Y"));
+    dfs_top(num_vex, &adj_list);
 }
